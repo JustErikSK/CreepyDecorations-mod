e7e57d7f54cc5d2e16e380fa46d1c6cf25f516b2
juste <sebo.erik@gmail.com>
2025-01-28 19:47:57 +0100

    Settings up gradle and initial code preparation

---
+++ src/main/java/me/minecraft/mod/creepydecorations/CreepyDecorations.java
@@ -0,0 +1,44 @@
+package me.minecraft.mod.creepydecorations;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+
+@Mod(CreepyDecorations.MOD_ID)
+public class CreepyDecorations {
+
+    public static final String MOD_ID = "creepydecorations";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreepyDecorations() {
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
